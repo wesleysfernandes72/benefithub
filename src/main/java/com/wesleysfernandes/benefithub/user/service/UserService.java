87fef36bdb7f7dc3365b2a70928cb92f1d8d5113
Wesley Fernandes <wesleysfernandes72@gmail.com>
2026-06-14 12:16:18 -0300

    setup inicial, começando autenticação

--- src/main/java/com/wesleysfernandes/benefithub/user/service/UserService.java
+++ src/main/java/com/wesleysfernandes/benefithub/user/service/UserService.java
@@ -4,18 +4,20 @@ import com.wesleysfernandes.benefithub.user.dto.CreateUserRequest;
 import com.wesleysfernandes.benefithub.user.entity.User;
 import com.wesleysfernandes.benefithub.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     public User create (CreateUserRequest request) {
 
         User user = User.builder()
                 .username(request.username())
-                .password(request.password())
+                .password(passwordEncoder.encode(request.password()))
                 .role(request.role())
                 .build();
 
